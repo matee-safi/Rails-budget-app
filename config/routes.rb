2e23204eb58fe86e3199491e4f9538ef0de11a09
Matee <matiullahsafibbc@gmail.com>
2023-09-15 16:20:29 +0430

    debug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   resources :users
   resources :groups, only: [:index, :show, :new, :create] do
     resources :entities, only: [:new, :create]
